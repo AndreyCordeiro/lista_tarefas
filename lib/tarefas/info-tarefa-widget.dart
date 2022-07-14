21eeec18e0480edbe72fea413e5ae6f25e29c4cc
Andrey <andreybr2002@hotmail.com>
2022-07-13 22:04:01 -0300

    Implementada a exclusão de tarefas

--- lib/tarefas/info-tarefa-widget.dart
+++ lib/tarefas/info-tarefa-widget.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lista_tarefas/tarefas/tarefa.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 
+import 'componentes/elevated-button-excluir.dart';
 import 'info-tarefa-page.dart';
 
 class InfoTarefaWidget extends State<InfoTarefaPage> {
@@ -16,80 +15,55 @@ class InfoTarefaWidget extends State<InfoTarefaPage> {
       appBar: AppBar(
         title: const Text("Detalhes da Tarefa"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
+      body: Container(
+        padding: const EdgeInsets.only(
+          left: 25,
+          top: 25,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
               children: <Widget>[
                 Column(
                   children: <Widget>[
                     Row(
                       children: <Widget>[
-                        const Text('Tarefa: '),
-                        const SizedBox(width: 30),
-                        Text(tarefa.nome),
+                        const Text('Tarefa: ',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        const SizedBox(width: 50),
+                        Text(
+                          tarefa.nome,
+                          overflow: TextOverflow.ellipsis,
+                        ),
                       ],
                     ),
                   ],
                 ),
               ],
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
+            const SizedBox(height: 35),
+            Row(
               children: <Widget>[
                 Column(
                   children: <Widget>[
                     Row(
                       children: <Widget>[
-                        const Text('Descrição: '),
+                        const Text('Descrição: ',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
                         const SizedBox(width: 30),
-                        Text(tarefa.descricao),
+                        Text(tarefa.descricao, overflow: TextOverflow.ellipsis),
                       ],
                     ),
                   ],
                 ),
               ],
             ),
-          ),
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: ElevatedButton(
-                  onPressed: () {
-
-                  },
-                  style: ButtonStyle(
-                    backgroundColor:
-                  ),
-                  child: const Text('Excluir'),
-                ),
-              ),
-            ],
-          ),
-        ],
+            const SizedBox(height: 30),
+            BotaoExcluir(tarefa: tarefa),
+          ],
+        ),
       ),
     );
-  }
-
-  Future<int> _salvar(String nome, String descricao, [int? id]) async {
-    String path = join(await getDatabasesPath(), 'task_list');
-    Database dataBase = await openDatabase(path, version: 1);
-
-    String sql;
-    Future<int> linhasAfetadas;
-
-    if (id == null) {
-      sql = "INSERT INTO tarefa (nome, descricao) VALUES (?, ?)";
-      linhasAfetadas = dataBase.rawInsert(sql, [nome, descricao]);
-    } else {
-      sql = "UPDATE tarefa SET nome = ?, descricao = ? WHERE id = ?";
-      linhasAfetadas = dataBase.rawInsert(sql, [nome, descricao]);
-    }
-    return linhasAfetadas;
   }
 }
